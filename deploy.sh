5e0c5495c4325f442c5cb458d65d3955cb87f6bf
Fawas <fawasrichard@gmail.com>
2024-04-12 16:35:30 +0100

    6 commit

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer
 sudo npm install -g pm2
 #stop any instance of our application running currently
-pm2 stop simplee_application
+pm2 stop SimpleeApplication
 #change directory into folder where application is downloaded
 cd SimpleeApplication/
 # Install application dependencies
@@ -11,4 +11,4 @@ npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 # Start the application with the process name example_app using pm2 .
-pm2 start ./bin/www --name simplee_application
+pm2 start ./bin/www --name SimpleeApplication
